905fd41e2e3ee8f82acae78a4e0ef6dbe49f0d79
Gonzalo Manzano <gonzalo040903@gmail.com>
2024-11-03 22:57:02 -0300

    mejoras en eliminar y modificar clientes

--- front/src/componentes/modificarcliente/modificarcliente.jsx
+++ front/src/componentes/modificarcliente/modificarcliente.jsx
@@ -25,6 +25,20 @@ import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 
 export function Modificarcliente() {
+    const calcular = (e) => {
+        e.preventDefault();
+        let monto = parseInt(document.getElementById("formMonto").value);
+        let intereses = parseInt(document.getElementById("formIntereses").value);
+        let montoFinal = (monto * intereses / 100) + monto
+        let semana = parseInt(document.getElementById("formSemanas").value);
+        let montoxsemana = montoFinal / semana;
+        let semanapaga = document.getElementById("formSemanaPaga")
+        let devuelve = document.getElementById("formDevuelve")
+        if (monto && intereses) {
+            semanapaga.value = montoxsemana;
+            devuelve.value = montoFinal;
+        }
+    }
     const [isGestionClientesOpen, setIsGestionClientesOpen] = useState(false);
     const [basicModal, setBasicModal] = useState(false);
     const toggleOpen = () => setBasicModal(!basicModal);
@@ -34,25 +48,11 @@ export function Modificarcliente() {
     const [semanas, setSemanas] = useState('1');
     const [monto, setMonto] = useState('');
     const [intereses, setIntereses] = useState('');
-    const calcularDevuelve = () => {
-        if (monto && intereses) {
-            const montoFloat = parseFloat(monto);
-            const interesFloat = (montoFloat * parseFloat(intereses)) / 100;
-            return (montoFloat + interesFloat).toFixed(2);
-        }
-        return '';
-    };
+
     const funcionSuccess = (e) => {
         e.preventDefault();
         toast.success('Nuevo Cliente Creado')
     }
-    // Calcula el pago semanal
-    const calcularPagoSemanal = () => {
-        if (monto && semanas) {
-            return (parseFloat(monto) / parseInt(semanas)).toFixed(2);
-        }
-        return '';
-    };
 
     const CustomInput = ({ label, type, id, value, onChange }) => (
         <MDBInput wrapperClass='mb-4' label={label} id={id} type={type} value={value} onChange={onChange} />
@@ -172,81 +172,96 @@ export function Modificarcliente() {
             {/* MODAL */}
             <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex='-1'>
                 <MDBModalDialog size="xl">
+                    <MDBModalContent>
 
 
 
+                        {/* FORMULARIO */}
+                        <div className="rounded-5 shadow-3 p-4 row" id="formulario">
+                            <MDBModalHeader className="mb-4">
+                                <h2 className="">Modificar Cliente</h2>
+                                <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
+                            </MDBModalHeader>
+                            <div className="col-5">
+                                <h3 className="text-center text-muted mb-3">Cliente</h3>
+                                <MDBRow>
+                                    <MDBCol col='3'>
+                                        <CustomInput label='Nombres' id='formNombre' type='text' />
+                                    </MDBCol>
+
+                                    <MDBCol col='3'>
+                                        <CustomInput label='Apellidos' id='formApellido' type='text' />
+                                    </MDBCol>
+                                </MDBRow>
+                                <CustomInput label='DNI' id='formDni' type='number' />
+                                <CustomInput label='Direccion' id='formDirec' type='text' />
+                                <MDBRow>
+                                    <MDBCol>
+                                        <CustomInput label='Telefono' id='formTel' type='number' />
+                                    </MDBCol>
+                                    <MDBCol>
+                                        <CustomInput label='Telefono 2' id='formTel2' type='number' />
+                                    </MDBCol>
+                                </MDBRow>
+
+                            </div>
+                            <div className="col-5">
+                                <h3 className="text-center text-muted mb-3">Prestamo</h3>
+                                <MDBRow>
+                                    <MDBCol col='3'>
+                                        <CustomInput label='Monto' id='formMonto' type='number' />
+                                    </MDBCol>
+
+                                    <MDBCol col='3'>
+                                        <CustomInput label='%Intereses' id='formIntereses' type='number' />
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBRow>
+                                    <MDBCol col='3'>
+                                        <CustomInput label='Fecha Inicio' id='formFecha' type='date' />
+                                    </MDBCol>
+
+                                    {/* Select de Semanas */}
+                                    <MDBCol col='3'>
+                                        <select id="formSemanas" className="form-select mb-4">
+                                            <option value={1}>1 Semana</option>
+                                            <option value={2}>2 Semanas</option>
+                                            <option value={3}>3 Semanas</option>
+                                            <option value={4}>4 Semanas</option>
+                                            <option value={5}>5 Semanas</option>
+                                            <option value={6}>6 Semanas</option>
+                                            <option value={7}>7 Semanas</option>
+                                            <option value={8}>8 Semanas</option>
+                                            <option value={9}>9 Semanas</option>
+                                            <option value={10}>10 Semanas</option>
+                                            <option value={11}>11 Semanas</option>
+                                            <option value={12}>12 Semanas</option>
 
 
-                    <div className="p-5 mx-4 mt-0 px-4 rounded-5 shadow-3 mb-4 row" id="formulario">
-                        <h3>Modificar Cliente</h3>
-                        <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
-                        <div className="col-5">
-                            <h3 className="text-center text-muted mb-3">Cliente</h3>
-                            <MDBRow>
-                                <MDBCol col='3'>
-                                    <CustomInput label='Nombres' id='formNombre' type='text' />
-                                </MDBCol>
-                                <MDBCol col='3'>
-                                    <CustomInput label='Apellidos' id='formApellido' type='text' />
-                                </MDBCol>
-                            </MDBRow>
-                            <CustomInput label='DNI' id='formDni' type='number' />
-                            <CustomInput label='Direccion' id='formDirec' type='text' />
-                            <MDBRow>
-                                <MDBCol>
-                                    <CustomInput label='Telefono' id='formTel' type='number' />
-                                </MDBCol>
-                                <MDBCol>
-                                    <CustomInput label='Telefono 2' id='formTel2' type='number' />
-                                </MDBCol>
-                            </MDBRow>
+                                        </select>
+                                    </MDBCol>
+                                </MDBRow>
+
+                                <MDBRow className="">
+                                    <MDBCol col='3'>
+                                        <MDBInput label="Monto Final" id="formDevuelve" value={" "} type="text" disabled />
+                                    </MDBCol>
+                                    <MDBCol col='3'>
+                                        <MDBInput label="Por Semana paga:" value={" "} id="formSemanaPaga" type="text" disabled />
+                                    </MDBCol>
+                                </MDBRow>
+                                <MDBRow>
+                                    <MDBCol col='3'>
+                                        <MDBBtn className='w-100 mt-3' href='/error' size='md' color="dark" onClick={calcular}>Calcular intereses</MDBBtn>
+                                    </MDBCol>
+                                </MDBRow>
+                            </div>
+
+                            <div className="col-6 mt-3">
+                                <MDBBtn className='w-100 mb-4' href='/error' color="warning" size='md' onClick={funcionSuccess}>Modificar Cliente</MDBBtn>
+                            </div>
                         </div>
-
-                        <div className="col-5">
-                            <h3 className="text-center text-muted mb-3">Prestamo</h3>
-                            <MDBRow>
-                                <MDBCol col='3'>
-                                    <CustomInput label='Monto' id='formMonto' type='number' value={monto} onChange={(e) => setMonto(e.target.value)} />
-                                </MDBCol>
-                                <MDBCol col='3'>
-                                    <CustomInput label='%Intereses' id='formIntereses' type='number' value={intereses} onChange={(e) => setIntereses(e.target.value)} />
-                                </MDBCol>
-                            </MDBRow>
-                            <MDBRow>
-                                <MDBCol col='3'>
-                                    <CustomInput label='Fecha Inicio' id='formFecha' type='date' />
-                                </MDBCol>
-                                <MDBCol col='3'>
-                                    <select className="form-select" value={semanas} onChange={(e) => setSemanas(e.target.value)}>
-                                        <option value="1">Semana 1</option>
-                                        <option value="2">Semana 2</option>
-                                        <option value="3">Semana 3</option>
-                                        <option value="4">Semana 4</option>
-                                        <option value="5">Semana 5</option>
-                                        <option value="6">Semana 6</option>
-                                        <option value="7">Semana 7</option>
-                                        <option value="8">Semana 8</option>
-                                        <option value="9">Semana 9</option>
-                                        <option value="10">Semana 10</option>
-                                        <option value="11">Semana 11</option>
-                                        <option value="12">Semana 12</option>
-                                    </select>
-                                </MDBCol>
-                            </MDBRow>
-                            <MDBRow className="mt-2">
-                                <MDBCol col='3'>
-                                    <label htmlFor="" className="text-muted">Devuelve</label>
-                                    <MDBInput label="" id="formDevuelve" type="text" value={calcularDevuelve()} disabled />
-                                </MDBCol>
-                                <MDBCol col='3'>
-                                    <label htmlFor="" className="text-muted">Cada semana paga</label>
-                                    <MDBInput label="" id="formControlDisabled" type="text" value={calcularPagoSemanal()} disabled />
-                                </MDBCol>
-                            </MDBRow>
-                        </div>
-                    
-                        <MDBBtn color="success">Guardar Cambios</MDBBtn>
-                    </div>
+                    </MDBModalContent>
                 </MDBModalDialog>
             </MDBModal>
         </MDBContainer >
