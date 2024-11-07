b8f124f10f74c5805e15f83c15813b24f7f84501
Josefi-me <josefinamedina513@gmail.com>
2024-11-07 17:01:24 -0300

    funciona tod xdlol

--- back/app.js
+++ back/app.js
@@ -11,12 +11,11 @@ const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-
 //conexion a MongoDB
-const uri = 'mongodb://localhost:27017/Sistema-Financiero';
+const uri = 'mongodb+srv://solheredia555:SistemaFinancieroFH@clustersistemafinancier.fxp2b.mongodb.net/Sistema-Financiero?retryWrites=true&w=majority';
 mongoose.connect(uri)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.log('Failed to connect to MongoDB', err));
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.log('Failed to connect to MongoDB Atlas', err));
 //Rutas de cliente y prestamo
 app.use('/api/clientes',  clientesRouter);
 app.use('/api/prestamos', prestamosRouter);
@@ -25,4 +24,4 @@ app.use('/api/prestamos', prestamosRouter);
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
 });
-//Pronto me mato
+
--- back/routes/routesP.js
+++ back/routes/routesP.js
@@ -4,42 +4,99 @@ import Cliente from '../model/modelCliente.js';
 
 const router = Router();
 
+//get q me trae todos los prestamos
+router.get('/', async (req, res) => {
+    try {
+        const prestamos = await Prestamo.find().populate('cliente', 'dni nombre apellido -_id');
+        res.json(prestamos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+//get por dni 
+router.get('/:dni', async (req, res) => {
+    try {
+        const cliente = await Cliente.findOne({ dni: req.params.dni });
+        
+        if (!cliente) {
+            return res.status(404).json({ message: 'Cliente no encontrado' });
+        }
+
+        const prestamos = await Prestamo.find({ cliente: cliente._id }).populate('cliente', 'dni nombre apellido -_id');
+        if (prestamos.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron préstamos para este cliente' });
+        }
+
+        res.json(prestamos);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
 //Crear prestamo
 router.post('/', async (req, res) => {
-    const {clienteId, monto, interes} = req.body;
+    const {dni, monto, semanas, intereses, fechaInicio} = req.body;
     try {
-        const cliente = await Cliente.findOne(clienteId);
+        const cliente = await Cliente.findOne({dni});
+
         if(!cliente) {
             return res.status(404).json({message:"'Cliente no encontrado"});
         }
         const nuevoPrestamo = new Prestamo({
-            cliente: clienteId,
+            cliente: cliente._id,
             monto,
             semanas,
-            interes,
-            fechaInicio: fechaInicio || Date.now(),
-
-        });
-        await nuevoPrestamo.save();
-        res.status(201).json(nuevoPrestamo)  
+            intereses,
+            fechaInicio: fechaInicio || Date.now()
+        }); 
+        await nuevoPrestamo.save(); 
         res.status(201).json(nuevoPrestamo);
     } catch (error) {
-        res.status(500).json({message: error.message})
         res.status(500).json({message: error.message});
     }
 });
 //actualizar prestamo(cuotas y montoadeudado)
-router.patch('/:id', async (req, res) => {
+router.patch('/:dni', async (req, res) => {
     const {cuotasPagadas} = req.body;
     try {
-        const prestamo = await Prestamo.findById(req.params.id);
-        if(!prestamo){
-            return res.status(404).json({message: 'Prestamo no encontrado'});
+        const cliente = await Cliente.findOne({ dni: req.params.dni});
+        if(!cliente){
+            return res.status(404).json({message: 'Cliente no encontrado'});
         }
+
+        const prestamo = await Prestamo.findOne({ cliente: cliente._id});
+        if (!prestamo){
+            return res.status(404).json({ message: 'Prestamo no encontrado para este cliente'});
+        }
+
         prestamo.cuotasPagadas = cuotasPagadas;
         prestamo.montoAdeudado = prestamo.montoFinal - ((prestamo.montoFinal / prestamo.semanas) * cuotasPagadas);
+        await prestamo.save();
+        res.json(prestamo);
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
+
+
+router.delete('/:dni', async (req, res) => {
+    try {
+        const cliente = await Cliente.findOne({ dni: req.params.dni});
+        if(!cliente){
+            return res.status(404).json({message: 'Cliente no encontrado'});
+        }
+
+        const result = await Prestamo.deleteMany({ cliente: cliente._id});
+        if (result.deletedCount === 0){
+            return res.status(404).json({message: 'No se encontraron prestamos para este cliente'});
+        }
+        res.json({message: 'Prestamo eliminado'});
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 export default router;
