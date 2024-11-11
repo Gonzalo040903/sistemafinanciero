f640528bfeba187493b97f257e4978b069f8eed0
Gonzalo Manzano <gonzalo040903@gmail.com>
2024-11-11 18:12:35 -0300

    elminiacion de modificar

--- front/src/componentes/vendedores/vendedores.jsx
+++ front/src/componentes/vendedores/vendedores.jsx
@@ -29,14 +29,12 @@ export function Vendedores() {
     const [optSmModal, setOptSmModal] = useState(false);
     const [isGestionClientesOpen, setIsGestionClientesOpen] = useState(false);
     const [basicModal, setBasicModal] = useState(false);
-    const [basicModal3, setBasicModal3] = useState(false);
     const [vendedores, setVendedores] = useState([]);
     const [vendedorAEliminar, setVendedorAEliminar] = useState(null);
 
     const toggleGestionClientes = () => {
         setIsGestionClientesOpen(!isGestionClientesOpen);
     };
-    const toggleOpen3 = () => setBasicModal3(!basicModal3);
     const toggleOpen2 = () => setOptSmModal(!optSmModal);
     const toggleOpen = () => setBasicModal(!basicModal);
 
@@ -160,7 +158,6 @@ export function Vendedores() {
                                     <th scope='col'>Nombre</th>
                                     <th scope='col'>Apellido</th>
                                     <th scope='col'></th>
-                                    <th scope='col'></th>
                                 </tr>
                             </MDBTableHead>
                             <MDBTableBody>
@@ -168,12 +165,6 @@ export function Vendedores() {
                                     <tr key={vendedor._id}>
                                         <td>{vendedor.nombre}</td>
                                         <td>{vendedor.apellido}</td>
-                                        <td>
-                                            <MDBBtn color='warning' size='sm' onClick={toggleOpen3}>
-                                                <MDBIcon icon="pencil-alt" className="me-2" />
-                                                Modificar
-                                            </MDBBtn>
-                                        </td>
                                         <td>
                                             <MDBBtn color='danger' size='sm' onClick={() => {
                                                 setVendedorAEliminar(vendedor); // Asignar el vendedor a eliminar
@@ -206,28 +197,6 @@ export function Vendedores() {
                                     <CustomInput label='Nombres' id='formNombre' type='text' />
                                     <CustomInput label='Apellidos' id='formApellido' type='text' />
                                     <MDBBtn className='w-100 mb-4' color="success" size='md' onClick={agregarVendedor}>Crear Vendedor</MDBBtn>
-                                </MDBRow>
-                            </div>
-                        </div>
-                    </MDBModalContent>
-                </MDBModalDialog>
-            </MDBModal>
-
-            {/* MODAL Modificar*/}
-            <MDBModal open={basicModal3} onClose={() => setBasicModal3(false)} tabIndex='-1'>
-                <MDBModalDialog size="md">
-                    <MDBModalContent>
-                        {/* FORMULARIO */}
-                        <div className="rounded-5 shadow-3 p-4 row" id="formulario">
-                            <MDBModalHeader className="mb-4">
-                                <h2 className=""><b>Modificar Vendedor</b></h2>
-                                <MDBBtn className='btn-close' color='none' onClick={toggleOpen3}></MDBBtn>
-                            </MDBModalHeader>
-                            <div className="col-10 my-3">
-                                <MDBRow>
-                                    <CustomInput label='Nombres' id='formNombre' type='text' />
-                                    <CustomInput label='Apellidos' id='formApellido' type='text' />
-                                    <MDBBtn className='w-100 mb-4' href='/error' color="warning" size='md'>Modificar Vendedor</MDBBtn>
                                 </MDBRow>
                             </div>
                         </div>
