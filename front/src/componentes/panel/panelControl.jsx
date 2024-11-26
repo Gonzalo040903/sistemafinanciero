6af0f87ec0cc73ef6d99f6e1cc34d44a2b788c75
Gonzalo Manzano <gonzalo040903@gmail.com>
2024-11-26 19:45:37 -0300

    test

--- front/src/componentes/panel/panelControl.jsx
+++ front/src/componentes/panel/panelControl.jsx
@@ -35,7 +35,7 @@ export function PanelControl() {
         // Llamada a la API para obtener clientes
         const fetchClientes = async () => {
             try {
-                const response = await axios.get('http://localhost:3001/api/clientes');
+                const response = await axios.get('https://sistemafinanciero-production.up.railway.app/api/clientes');
                 setClientes(response.data);
             } catch (error) {
                 console.error("Error al obtener los clientes:", error);
